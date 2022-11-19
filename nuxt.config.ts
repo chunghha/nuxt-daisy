33ea1b5832bb03cb10c409240b874a8ff30d76f7
Chris Ha <chunghha@users.noreply.github.com>
2022-11-19 05:26:13 -0600

    build(lib): nuxt v3.0.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: [
+  modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     '@vueuse/nuxt',
